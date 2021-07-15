9676da91c9099b04a5b6b92d398fb95175aa8865
Verenasworld <verena.felizidas@gmail.com>
2021-07-15 14:48:08 +0200

    add on card click flip sound

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-cards',
@@ -9,74 +11,114 @@ export class CardsComponent implements OnInit {
 
 
   visible= true;
-  card = Array.from(document.getElementsByClassName('card'));
-  matchsound = Audio;
-  flipsound = Audio;
   timecounter = 100;
+ 
+  
+  //card = Array.from(document.getElementsByClassName('card'));
+
+//Audio
+  flipsound =  new Audio ('./assets/audio/flip.wav');
+  matchsound = new Audio ('src/assets/audio/mixkit-retro-game-notification-212.wav');
+  winsound = new Audio ('src/assets/audio/mixkit-auditorium-moderate-applause-and-cheering-502-wav')
 
   
 
-
-
-  constructor () { 
-    
-  }
+ constructor () { 
+   }
 
   ngOnInit(): void {
-  
   }
- 
-  clickstart(){
-  this.visible =  false
-  console.log('this is working') ;
 
+ 
+clickstart(){
+  this.clickcard()
+  this.visible =  false;
   let intervalId = setInterval(() => {
     this.timecounter = this.timecounter - 1;
-    console.log(this.timecounter)
     if(this.timecounter === 0) clearInterval(intervalId)
-}, 1000)
-  console.log(this.timecounter) ;
-  }
-
-
-ready(){
-  this.card.forEach(card => {
-    card.addEventListener('click', ()=>{
-      console.log('click');
-      // i think this is not working !! 
-    })
-  })
+}, 1000);
 }
 
 
 flip(){
-  //flip sound & flip counter
+  this.flipsound.play();
 }
+
 
 match(){
-// match sound 
-}
-
-gameOver(){
-
-//show overlay game over & button restart 
-}
-
-
-counter(){
-  // sobald die erste Karte geflipt ist fange an von 100 runterzählen time=100 -1 , 
-  //wenn die Zeit abgelaufen ist und nicht alle karten umgedreht worden ist zeige Game Over - button restart 
-  
+  this.matchsound.play();
 }
 
 win(){
+  this.winsound.play();
+}
+
+
+ clickcard(){
+  let cards = Array.from(document.getElementsByClassName('card'));
+
+
+  cards.forEach(card => {
+    card.addEventListener('click', ()=>{
+      console.log('card is working')
+      this.flip();
+    })
+    
+  });
+}
+
+
+
+}
+
+
+
+
+
+   // restart = 
+// if timecounter = 0 show visible on overlay txt id game over txt 
+//
+
+
+
+
+
+
+ //ready(){
+  // this.card.forEach(card => {
+   //  card.addEventListener('click', ()=>{
+
+   //    console.log('click');
+      // i think this is not working !! 
+   //  })
+  //  })
+ //}
+
+
+//flip(){
+  //flip sound & flip counter
+//}
+
+//match(){
+// match sound 
+//}
+
+ //gameOver(){
+
+//show overlay game over & button restart 
+ //}
+
+
+//counter(){
+  // sobald die erste Karte geflipt ist fange an von 100 runterzählen time=100 -1 , 
+  //wenn die Zeit abgelaufen ist und nicht alle karten umgedreht worden ist zeige Game Over - button restart 
+  
+//}
+
+//win(){
 // wenn alle karten gemacht sind zeige gewonnen 
 // show overlay win & button to restart 
 
-}
-
-
-}
 
 
 //can i take the images in to array and loop random to it for showing on game start , restart all random images ????? 
