79770e786d9668c89112f62bf2915427c44315d7
Verenasworld <verena.felizidas@gmail.com>
2021-07-15 12:21:52 +0200

    add basic counter to the cards

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -7,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
+
   visible= true;
   card = Array.from(document.getElementsByClassName('card'));
-   matchsound = Audio;
-   flipsound = Audio;
+  matchsound = Audio;
+  flipsound = Audio;
+  timecounter = 100;
+
+  
 
 
-  constructor() { 
+
+  constructor () { 
     
   }
 
@@ -22,8 +27,15 @@ export class CardsComponent implements OnInit {
   }
  
   clickstart(){
-  this.visible = false
-  console.log('this is working')  
+  this.visible =  false
+  console.log('this is working') ;
+
+  let intervalId = setInterval(() => {
+    this.timecounter = this.timecounter - 1;
+    console.log(this.timecounter)
+    if(this.timecounter === 0) clearInterval(intervalId)
+}, 1000)
+  console.log(this.timecounter) ;
   }
 
 
@@ -35,6 +47,7 @@ ready(){
     })
   })
 }
+
 
 flip(){
   //flip sound & flip counter
@@ -50,16 +63,21 @@ gameOver(){
 }
 
 
-timer(){
-// from 100 to 0 counter -
+counter(){
+  // sobald die erste Karte geflipt ist fange an von 100 runterzählen time=100 -1 , 
+  //wenn die Zeit abgelaufen ist und nicht alle karten umgedreht worden ist zeige Game Over - button restart 
+  
 }
 
 win(){
-
+// wenn alle karten gemacht sind zeige gewonnen 
 // show overlay win & button to restart 
-}
-
 
 }
 
 
+}
+
+
+//can i take the images in to array and loop random to it for showing on game start , restart all random images ????? 
+//how i can do it ? img = new [ fill i with font face image ? ]
